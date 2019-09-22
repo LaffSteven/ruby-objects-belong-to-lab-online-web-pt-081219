bd455fadccc34030a9b9d6f7eb6696f2e1010cbe
Steven Laff <steve.laff@gmail.com>
2019-09-22 14:42:34 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,19 @@
 class Post
   
+  attr_accessor :title, :author
+  
   @@all_posts
   
+  def initialize(title, author)
+    @title = title
+    @author = assign_author
+  end
   
+  def assign_author(author)
+    if Author.all_authors.detect(author)
+      author
+    else
+      Author.new(author)
+    end
+  end   
 end
